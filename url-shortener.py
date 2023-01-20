d8e0f2329c5035a577bcd392814f9eef7ed33d2f
alvinja81 <alvinja8845@gmail.com>
2023-01-20 13:02:46 +0330

    new

--- url-shortener.py
+++ url-shortener.py
@@ -1,8 +1,8 @@
-import bitly_api
+import bitly_api as bitly
  
 BITLY_ACCESS_TOKEN ="ACCESS_TOKEN"
  
-b = bitly_api.Connection(access_token = BITLY_ACCESS_TOKEN)
+b = bitly.Connection(access_token = BITLY_ACCESS_TOKEN)
  
 response = b.shorten('http://google.com/')
 print(response)
