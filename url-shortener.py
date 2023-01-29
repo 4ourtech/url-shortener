973fe48efd2b8b2835ba6f99419a02a2b99886a4
alvinja81 <alvinja8845@gmail.com>
2023-01-29 23:40:46 +0330

    renewe with new method and framework(django)

--- url-shortener.py
+++
@@ -1,6 +0,0 @@
-import pyshorteners as sh
-link = 'https://www.youtube.com/@4ourtech'
-s = sh.Shortener()
-
-print(s.tinyurl.short(link))
-
---
+++ url-shortener/url_shortener/url_shorty/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UrlShortyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'url_shorty'
