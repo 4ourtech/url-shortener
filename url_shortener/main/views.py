faca8b3ab10e7c8130c33e6482cc195177c1e5aa
alvinja81 <alvinja8845@gmail.com>
2023-01-30 01:22:25 +0330

    semi-final projects

--- url_shortener/main/views.py
+++ url_shortener/main/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 import pyshorteners
 
 # Create your views here.
+def shorten(request,url):
+    shortener = pyshorteners.Shortener()
+    shortened_url = shortener.chilpit.short(url)
+    return HttpResponse(f'shortened URL: <a href="{shortened_url}">{shortened_url}</a>')
